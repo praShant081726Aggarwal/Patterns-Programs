ef3343845a2e3a1338f4a011079305c7a7477ca0
unknown <aggarwalprashant235@gmail.com>
2024-01-25 13:52:31 +0530

    Right Triangle

---
+++ _02RightTriangle.java
@@ -0,0 +1,14 @@
+/**
+ * _02RightTriangle
+ */
+public class _02RightTriangle {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

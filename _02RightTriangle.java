e3cd6224b0ccfaf09f65a9d8185d7b45c35e5117
unknown <aggarwalprashant235@gmail.com>
2024-01-25 13:55:15 +0530

    Right triangle

--- _02RightTriangle.java
+++ _02RightTriangle.java
@@ -4,7 +4,8 @@
 public class _02RightTriangle {
 
     public static void main(String[] args) {
-        for(int i=1; i<=5; i++){
+        int n = 5;
+        for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
                 System.out.print("*");
             }
